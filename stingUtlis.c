d1f83552d7ec757fb64d2d10d42bcd26f5a1511a
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-20 21:50:07 +0500

    string utils.c file

---
+++ stingUtlis.c
@@ -0,0 +1,8 @@
+//string utlis.c
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include"string_utils.h"
+// Function: replace charater in the string
+void replaceChar(char *name, char oldChar, char newChar) 
